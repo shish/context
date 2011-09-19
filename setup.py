e3a137663e2e8edbac03bf82f25315efb0ab072d
Shish <shish@shishnet.org>
2011-09-19 22:57:46 +0100

    py2exe support

--- setup.py
+++ setup.py
@@ -1,20 +1,49 @@
 from distutils.core import setup
 from distutils.extension import Extension
+
 try:
     from Cython.Distutils import build_ext
 except ImportError:
     build_ext = None
 
+try:
+    import py2exe
+except ImportError:
+    pass
+
+
 setup(
+    # metadata
     name = 'Context',
     version = "0.9.0",
     author = "Shish",
     author_email = "shish@civicboom.com",
     url = "http://www.boomtools.co.uk/context",
-    cmdclass = {'build_ext': build_ext},
 
-    scripts = ["launcher", ],
-    ext_modules = [Extension("contextview", ["contextview.py"]), ],
+    # cython
+    cmdclass = {'build_ext': build_ext},
+    #ext_modules = [Extension("contextview", ["contextview.py"]), ],
+    py_modules = ["contextview", ],
+
+    # py2exe
+    windows = [
+        {
+            "script": "context",
+            "icon_resources": [(1, "images/boomtools.ico")],
+        }
+    ],
+    zipfile = None,
+    options = {
+        "py2exe": {
+            "optimize": 2,
+            "bundle_files": 3, # 1 (bundle all) would be nicer, but no win64 support :(
+            "compressed": True,
+            "excludes": ["doctest", "pdb", "unittest", "difflib", "inspect"],
+        }
+    },
+
+    # common
+    scripts = ["context", ],
     data_files = [
         ('images', ['images/boomtools.ico', 'images/end.gif', 'images/next.gif', 'images/prev.gif', 'images/start.gif']),
         ('api', ["context.py", ]),
