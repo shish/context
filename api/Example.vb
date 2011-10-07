2a5f68104f850a5579a75ec95fed4db0b13dd504
Shish <shish@shishnet.org>
2011-10-07 13:21:11 +0100

    VB example

---
+++ api/Example.vb
@@ -0,0 +1,24 @@
+﻿Module Example
+    Private Sub hello()
+        Context.start("saying hello")
+        Console.Write("hello ")
+        Threading.Thread.Sleep(1000)
+        Context.endok("saying hello")
+    End Sub
+
+    Private Sub world()
+        Context.start("saying world")
+        Console.WriteLine("world")
+        Threading.Thread.Sleep(2000)
+        Context.endok("saying world")
+    End Sub
+
+    Sub Main()
+        Context.setLog("output.vb.ctext")
+        Context.start("running program")
+        hello()
+        world()
+        Context.endok("running program")
+        Context.closeLog()
+    End Sub
+End Module
