491f78bdd2d8f29d5c4b40d312ab78267d77bcd5
Shish <shish@shishnet.org>
2011-10-12 16:37:20 +0100

    remove unnecessary bits from context.h

--- api/context.h
+++ api/context.h
@@ -1,12 +1,8 @@
-#include <stdio.h>
-
 #define ctx_log_bmark(text) ctx_log_msg(__FUNCTION__, text, "BMARK")
 #define ctx_log_start(text) ctx_log_msg(__FUNCTION__, text, "START")
 #define ctx_log_endok(text) ctx_log_msg(__FUNCTION__, text, "ENDOK")
 #define ctx_log_ender(text) ctx_log_msg(__FUNCTION__, text, "ENDER")
 #define ctx_log_clear(text) ctx_log_msg(__FUNCTION__, text, "CLEAR")
-
-extern FILE *context_log;
 
 extern void ctx_set_log(const char *name);
 extern void ctx_log_msg(const char *func, const char *text, const char *type);
