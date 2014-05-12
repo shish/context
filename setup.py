afb25c66b482934d17efb20e1c4d745640f03307
Shish <shish@shishnet.org>
2014-05-12 22:05:18 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='context.viewer',
+    version='0.0',
+    description='Context Viewer',
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    author='Shish',
+    author_email='shish+context@shishnet.org',
+    url='http://code.shishnet.org/context',
+    keywords='profile',
+    packages=["context"],
+    namespace_packages=["context"],
+    test_suite = "context.viewer.tests",
+    zip_safe=True,
+    install_requires=[
+        "decorator",
+    ],
+    entry_points="""\
+    [console_scripts]
+    context-viewer = context.viewer.main:main
+    context-compiler = context.compiler:main
+    """,
+)
+
